22705e355cf7dd74fe54b96ef0d37b14128a7c70
Ned Tesco <nedtesco@gmail.com>
2024-03-31 17:26:10 +0100

    Comments added to finance_calculators.py

--- finance_calculators.py
+++ finance_calculators.py
@@ -2,12 +2,17 @@ import math
 
 valid_response = False
 
+# Creating a loop that will continue to prompt the user for input until a
+# valid response is provided.
 while valid_response == False:
     finance_option = input("""investment - to calculate the amount of interest you'll earn on your investment
 bond - to calculate the amount you'll have to pay on a home loan
 Enter either 'investment' or 'bond' from the menu above to proceed:
 > """).lower()
 
+    # If user chooses 'investment', they will be asked to enter a set of variables
+    # to be calculated, as well as what type of interest which will dictate the nature
+    # of the calculation
     if finance_option == "investment":
         valid_response = True
         deposit = int(input("Please enter your deposit amount (in dollars): "))
@@ -23,6 +28,8 @@ Enter either 'investment' or 'bond' from the menu above to proceed:
 
         print(f"Total amount after investment: ${investment_result}")
 
+    # If user chooses 'bond', they will be asked to enter a different set of variables
+    # to be calculated.
     elif finance_option == "bond":
         valid_response = True
         value = int(input("Please enter the value of your home (in $): "))
@@ -32,5 +39,6 @@ Enter either 'investment' or 'bond' from the menu above to proceed:
 
         print(f"Monthly repayment amount: ${bond_repayment}")
 
+    # User told if input is invalid and allowed to re-enter a response.
     else:
         print("Error - Please enter a valid response.")
